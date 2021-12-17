5651d1876be3df23b254b7502e95dd60bfd8e171
SandhyR <sandhyraihan36@gmail.com>
2021-12-17 12:34:49 +0700

    api4

--- src/lukeeey/waterdog/Main.php
+++ src/lukeeey/waterdog/Main.php
@@ -2,40 +2,35 @@
 declare ( strict_types = 1 );
 namespace lukeeey\waterdog;
 
+use pocketmine\event\player\PlayerLoginEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\event\server\DataPacketReceiveEvent;
-use pocketmine\network\mcpe\protocol\LoginPacket;
-use pocketmine\network\mcpe\RakLibInterface;
-use ReflectionProperty;
-use ReflectionException;
+use pocketmine\network\mcpe\raklib\RakLibInterface;
 use ReflectionClass;
 
 class Main extends PluginBase implements Listener {
 
-    public function onEnable() {
+    public function onEnable() : void{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
-
-    public function onPacketReceive(DataPacketReceiveEvent $event): void {
-        $packet = $event->getPacket();
-        if($packet instanceof LoginPacket) {   
-                foreach ( $this->getServer()->getNetwork()->getInterfaces() as $interface ) {
+public function onLogin(PlayerLoginEvent $event){
+        $clientdata = $event->getPlayer()->getPlayerInfo()->getExtraData();
+	foreach ( $this->getServer()->getNetwork()->getInterfaces() as $interface ) {
 						  if ( $interface instanceof RakLibInterface ) {
-								try {
-									 $reflector = new \ReflectionProperty( $interface, "interface" );
-									 $reflector->setAccessible( true );
-									 $reflector->getValue( $interface )->sendOption( "packetLimit", 900000000000 );
-								} catch ( ReflectionException $e ) {}
+								$interface->setPacketLimit(900000000000);
 						  }
 					 }
-            if(isset($packet->clientData["WaterDog_RemoteIP"])) {
-                $class = new ReflectionClass($event->getPlayer());
-
+        if(isset($clientdata["Waterdog_IP"])){
+			$class = new ReflectionClass($event->getPlayer()->getNetworkSession());
                 $prop = $class->getProperty("ip");
                 $prop->setAccessible(true);
-                $prop->setValue($event->getPlayer(), $packet->clientData["WaterDog_RemoteIP"]);
-            }
+                $prop->setValue($event->getPlayer()->getNetworkSession(), $clientdata["Waterdog_IP"]);
         }
-    }
+	if(isset($clientdata["Waterdog_XUID"])){
+		$class = new ReflectionClass($event->getPlayer());
+		$prop = $class->getProperty("xuid");
+		$prop->setAccessible(true);
+		$prop->setValue($event->getPlayer(), $clientdata["Waterdog_XUID"]);
+	}
+}
 }
