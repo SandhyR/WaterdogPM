11351bc40950bfe100d550cb984e0a7b7f7f8987
SandhyR <sandhyraihan36@gmail.com>
2021-12-17 12:49:31 +0700

    Update Main.php

--- src/lukeeey/waterdog/Main.php
+++ src/lukeeey/waterdog/Main.php
@@ -13,24 +13,25 @@ class Main extends PluginBase implements Listener {
     public function onEnable() : void{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
-public function onLogin(PlayerLoginEvent $event){
-        $clientdata = $event->getPlayer()->getPlayerInfo()->getExtraData();
-	foreach ( $this->getServer()->getNetwork()->getInterfaces() as $interface ) {
-						  if ( $interface instanceof RakLibInterface ) {
-								$interface->setPacketLimit(900000000000);
-						  }
-					 }
-        if(isset($clientdata["Waterdog_IP"])){
+	public function onLogin(PlayerLoginEvent $event)
+	{
+		$clientdata = $event->getPlayer()->getPlayerInfo()->getExtraData();
+		foreach ($this->getServer()->getNetwork()->getInterfaces() as $interface) {
+			if ($interface instanceof RakLibInterface) {
+				$interface->setPacketLimit(900000000000);
+			}
+		}
+		if (isset($clientdata["Waterdog_IP"])) {
 			$class = new ReflectionClass($event->getPlayer()->getNetworkSession());
-                $prop = $class->getProperty("ip");
-                $prop->setAccessible(true);
-                $prop->setValue($event->getPlayer()->getNetworkSession(), $clientdata["Waterdog_IP"]);
-        }
-	if(isset($clientdata["Waterdog_XUID"])){
-		$class = new ReflectionClass($event->getPlayer());
-		$prop = $class->getProperty("xuid");
-		$prop->setAccessible(true);
-		$prop->setValue($event->getPlayer(), $clientdata["Waterdog_XUID"]);
+			$prop = $class->getProperty("ip");
+			$prop->setAccessible(true);
+			$prop->setValue($event->getPlayer()->getNetworkSession(), $clientdata["Waterdog_IP"]);
+		}
+		if (isset($clientdata["Waterdog_XUID"])) {
+			$class = new ReflectionClass($event->getPlayer());
+			$prop = $class->getProperty("xuid");
+			$prop->setAccessible(true);
+			$prop->setValue($event->getPlayer(), $clientdata["Waterdog_XUID"]);
+		}
 	}
-}
 }
